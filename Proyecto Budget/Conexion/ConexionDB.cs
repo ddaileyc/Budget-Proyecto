e914443e75f147863fec1046d656be3f8258dcca
Diego S <DiegoSR1693@users.noreply.github.com>
2017-06-10 16:28:37 -0600

    Se agrega clase conexion

---
+++ Proyecto Budget/Conexion/ConexionDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Budget.Conexion
+{
+    class ConexionDB
+    {
+
+
+        // public static string conexion = "Data Source=(local); Initial Catalog=Programacion4; Integrated Security=true";
+
+        public static string conexion = "Server=tcp:progra4.database.windows.net,1433;Database=Progra4;User ID=progra4;Password=progra.4 ;Encrypt=True";
+
+    }
+}

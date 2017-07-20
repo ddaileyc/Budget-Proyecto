b7a20092c1d8e838244d5cc88f61fe9afc6a1bc3
Donald Dailey - Home <ddailey@outlook.com>
2017-07-19 22:19:47 -0600

    Logueo contra base de datos y flujo sin bugs

---
+++ Proyecto Budget/Control/CRUD_Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data; // para trabajar con datos de sql
+using System.Data.SqlClient; // para poder enviar comando desde visual a sql
+
+namespace Proyecto_Budget.Control
+{
+    class CRUD_Usuario
+    {
+        public static Conexion.ConexionDB controlDBUsuario = new Conexion.ConexionDB();
+
+        public Boolean ValidarUsuario(string usuario, string contrasena)
+        {
+            return controlDBUsuario.ValidarUsuario(usuario, contrasena);
+        }
+
+        public static DataTable MostrarUsuarios()
+        {
+            DataTable DtUsuarios = new DataTable("Usuarios");
+            controlDBUsuario.MostrarProveedores(DtUsuarios);
+            return DtUsuarios;
+        }
+    }
+}
